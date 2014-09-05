1601dc34473c59984b3d19c9097f67d9a9faff65
pyankoff <pyankoff@gmail.com>
2014-09-05 12:37:15 +0400

    scrolling added

--- client/views/landing.js
+++ client/views/landing.js
@@ -1,7 +1,20 @@
 Template.landing.events({
-  'click #moreClick': function (evt) {
+  'click .moreClick': function (evt) {
     $('html, body').animate({
     scrollTop: $("#more").offset().top
+    }, 400);
+  }
+});
+
+Template.header.events({
+  'click .moreClick': function (evt) {
+    $('html, body').animate({
+    scrollTop: $("#more").offset().top
+    }, 400);
+  },
+  'click .contactsClick': function (evt) {
+    $('html, body').animate({
+    scrollTop: $("#contacts").offset().top
     }, 400);
   }
 });
